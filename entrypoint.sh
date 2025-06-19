6cf2d1d9b22ccadb2e9788d33c592712c09234fd
daboe01 <daboe01@googlemail.com>
2025-06-19 12:37:09 +0200

    new: hint towards ollama support

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-/etc/init.d/postgresql start && hypnotoad -f /app/backend.pl # && ollama serve
+/etc/init.d/postgresql start && hypnotoad -f /app/backend.pl
+
+# ollama support would go like this:
+# /etc/init.d/postgresql start && hypnotoad /app/backend.pl && ollama serve
